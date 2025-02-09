c304f6afc064506537a8c739501b76d5969a4c90
Viet Hung Dang <kitdang123@gmail.com>
2025-02-09 18:36:02 +0000

    Remove some stuff

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import ValentineComponent from './components/ValentineComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
       <ValentineComponent/>
   )
