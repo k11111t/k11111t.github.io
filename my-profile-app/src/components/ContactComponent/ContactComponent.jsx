c39e7be789f583a400532724371883efbfa4dc68
Viet Hung Dang <kitdang123@gmail.com>
2025-05-18 16:56:17 +0100

    Add projects

--- my-profile-app/src/components/ContactComponent/ContactComponent.jsx
+++ my-profile-app/src/components/ContactComponent/ContactComponent.jsx
@@ -7,24 +7,32 @@ const ContactComponent = () => {
             <h1 className={styles.header}  id="contact">Contact Me</h1>
             <div className={styles.description}>
                 <span >Email:</span>
-                <a href="mailto:your-email@example.com" >
-                    your-email@example.com
+                <a 
+                    className={styles.whiteLink}
+                    href="mailto:vhdang0@gmail.com" >
+                    vhdang0@gmail.com
                 </a>
             </div>
             <div className={styles.description}>
                 <span>LinkedIn:</span>
                 <a
-                    href="https://www.linkedin.com/in/your-profile"
+                    className={styles.whiteLink}
+                    href="https://www.linkedin.com/in/viet-hung-dang/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    linkedin.com/in/your-profile
+                    linkedin.com/in/viet-hung-dang/
                 </a>
             </div>
             <div className={styles.description}>
-                <span>Phone:</span>
-                <a href="tel:+1234567890">
-                    +1 (234) 567-890
+                <span>GitHub:</span>
+                <a
+                    className={styles.whiteLink}
+                    href="https://www.github.com/k11111t"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    github.com/k11111t
                 </a>
             </div>
         </section>
--- my-profile-app/src/components/ProjectsComponent/ProjectsComponent.jsx
+++ my-profile-app/src/components/ProjectsComponent/ProjectsComponent.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
 import styles from './ProjectsComponent.module.css';
+import virtualMapDemoImage from '../../assets/3rd year project/project_demo.png';
+import deckedOutDemoImage from '../../assets/1st year project/project_demo.png';
 
 const ProjectsComponent = () => {
     return (
         <section id="projects">
             <h1 className={styles.header}>Projects</h1>
-
-            <h2 className={styles.subheader}>
-                Virtual Map
-            </h2>
-            <p className={styles.description}>
-                This is a virtual map application developed using Javascript, Node.js , PHP. 
-                It allows users to explore different locations and view information about them. 
-                The project uses Mapbox API for displaying maps and has a clean and responsive UI.
-            </p>
-
-            <h2 className={styles.subheader}>
-                Multiplayer Card game
-            </h2>
-            <p className={styles.description}>
-                This is a multiplayer card game developed using Node.js, PHP, Javascript. 
-                It allows users to play card games with friends in real-time. 
-                The project uses WebSocket for real-time communication and has a clean and responsive UI.
-            </p>
-
+            <div className={styles.projectWrapper}>
+                <h2 className={styles.subheader}>
+                <a className={styles.whiteLink} target="_blank" href={"https://github.com/k11111t/3rd-year-project"}>Virtual Map of Kilburn</a>
+                </h2>
+                <p className={styles.description}>
+                    This is a virtual map application developed using Javascript, Node.js, PHP, SQL. 
+                    It allows users to explore different locations in the Computer Science building of the University of Manchester.
+                    The project uses Mapbox API for displaying maps and has a clean and responsive UI.
+                    The user can view information, search for rooms, and find shortest path between them.
+                </p>
+                <div className={styles.demoImageDiv}>
+                    <img className={styles.demoImage} src={virtualMapDemoImage}/>
+                </div>
+            </div>
+            
+            <div className={styles.projectWrapper}>
+                <h2 className={styles.subheader}>
+                <a className={styles.whiteLink} target="_blank" href={"https://github.com/k11111t/1st-year-project"}>Decked Out</a>
+                </h2>
+                <p className={styles.description}>
+                    This is a multiplayer card game developed using Node.js, PHP, Javascript, SQL. 
+                    It allows users to play Blackjack with friends in real-time. 
+                    The project uses WebSocket for real-time communication and has a clean and responsive UI.
+                </p>
+                <div className={styles.demoImageDiv}>
+                    <img className={styles.demoImage} src={deckedOutDemoImage}/>
+                </div>
+            </div>
         </section>
-
     );
 };
 
